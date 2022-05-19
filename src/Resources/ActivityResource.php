f2b71cae4a61efa6e2dd38d9851db4e4a83116f9
Ziyaan Hassan <ziyaan2010@gmail.com>
2022-05-19 13:05:35 +0500

    remove shouldRegisterNavigation

--- src/Resources/ActivityResource.php
+++ src/Resources/ActivityResource.php
@@ -255,9 +255,4 @@ class ActivityResource extends Resource
     {
         return __('filament-logger::filament-logger.nav.log.icon');
     }
-
-    protected static function shouldRegisterNavigation(): bool
-    {
-        return auth()->user()->can('view_any_activity');
-    }
 }
