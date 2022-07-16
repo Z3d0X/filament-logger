a7d4d7e9c9b8e9e926bd47b920abdaa0a687c04f
Pathros <latlovs-github@yahoo.com>
2022-07-15 20:57:54 -0500

    Added basic Spanish translations

---
+++ resources/lang/es/filament-logger.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Settings',
+    'nav.log.label' => 'Bitácora de actividad',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'Registro de actividad',
+    'resource.label.logs' => 'Registros de actividad',
+
+];
