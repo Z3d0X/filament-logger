81b7922e3465ce0f6143d96e42856555d09a1b0a
Nicolas <nicolas.baud12@gmail.com>
2022-07-19 17:35:06 +0200

    Create filament-logger.php
    Add French Translation

---
+++ resources/lang/fr/filament-logger.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Paramètres',
+    'nav.log.label' => 'Journal d\'activités',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'Journal d\'activité',
+    'resource.label.logs' => 'Journal d\'activités',
+    'resource.label.user' => 'Utilisateur',
+    'resource.label.subject' => 'Sujet',
+    'resource.label.subject_type' => 'Type de sujet',
+    'resource.label.description' => 'Description',
+    'resource.label.type' => 'Type',
+    'resource.label.event' => 'Événement',
+    'resource.label.logged_at' => 'Enregistré à',
+    'resource.label.properties' => 'Propriétés',    
+
+];
