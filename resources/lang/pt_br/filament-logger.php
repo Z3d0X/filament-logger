846f75a2da0ceea8e1ce7f9f7ba4a949c0bdb7d0
Gonçalo Franco <gapfranco@gmail.com>
2022-08-03 13:28:47 -0300

    Brazilian portuguse translation

---
+++ resources/lang/pt_br/filament-logger.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Settings',
+    'nav.log.label' => 'Registro de atividade',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'Registro de atividade',
+    'resource.label.logs' => 'Registros de atividade',
+    'resource.label.user' => 'Usuário',
+    'resource.label.subject' => 'Assunto',
+    'resource.label.subject_type' => 'Tipo de assunto',
+    'resource.label.description' => 'Descrição',
+    'resource.label.type' => 'Tipo',
+    'resource.label.event' => 'Evento',
+    'resource.label.logged_at' => 'Registrado em',
+    'resource.label.properties' => 'Propriedades',
+
+];
