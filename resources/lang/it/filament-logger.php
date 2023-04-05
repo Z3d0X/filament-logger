c97bc5f94d079a26311e14aa4a89d0af4cdc90b9
Marco Guidolin <guidolin.marco94@gmail.com>
2023-04-05 10:38:57 +0000

    ADD Italian translations

---
+++ resources/lang/it/filament-logger.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Impostazioni',
+    'nav.log.label' => 'Registro attività',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'Log',
+    'resource.label.logs' => 'Log',
+    'resource.label.user' => 'User',
+    'resource.label.subject' => 'Soggetto',
+    'resource.label.subject_type' => 'Tipo di soggetto',
+    'resource.label.description' => 'Descrizione',
+    'resource.label.type' => 'Tipologia',
+    'resource.label.event' => 'Evento',
+    'resource.label.logged_at' => 'Registrato Il',
+    'resource.label.properties' => 'Proprietà',
+    'resource.label.old' => 'Prima',
+    'resource.label.new' => 'Ora',
+    'resource.label.old_value' => 'Valore Precedente',
+    'resource.label.new_value' => 'Nuovo Valore',
+];
