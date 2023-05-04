0f4111137162ea19902361da0b065a5bdabe372b
AmirAghaee <amir.aghaee73@gmail.com>
2023-05-04 08:31:14 +0330

    Add Persian Lang (#50)
    add Persian lang

---
+++ resources/lang/fa/filament-logger.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | Navigation & Resource
+  |--------------------------------------------------------------------------
+  */
+
+    'nav.group' => 'تنظیمات',
+    'nav.log.label' => 'لاگ فعالیت ها',
+  'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'لاگ فعالیت',
+    'resource.label.logs' => 'لاگ فعالیت ها',
+    'resource.label.user' => 'کاربر',
+    'resource.label.subject' => 'موضوع',
+    'resource.label.subject_type' => 'نوع موضوع',
+    'resource.label.description' => 'شرح',
+    'resource.label.type' => 'نوع',
+    'resource.label.event' => 'رویداد',
+    'resource.label.logged_at' => 'لاگ شده در',
+    'resource.label.properties' => 'خصوصیات',
+    'resource.label.old' => 'قدیمی',
+    'resource.label.new' => 'جدید',
+    'resource.label.old_value' => 'مقدار قدیمی',
+    'resource.label.new_value' => 'مقدار جدید',
+];
