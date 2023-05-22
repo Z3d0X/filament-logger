f40a45923577f35ac62e53721667db7a4a9f99a2
ruswan <sukabumikode@gmail.com>
2023-05-22 11:19:49 +0700

    Indonesian translation

---
+++ resources/lang/id/filament-logger.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Pengaturan',
+    'nav.log.label' => 'Catatan Aktivitas',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'Catatan aktivitas',
+    'resource.label.logs' => 'Catatan aktivitas',
+    'resource.label.user' => 'Pengguna',
+    'resource.label.subject' => 'Subjek',
+    'resource.label.subject_type' => 'Tipe Subjek',
+    'resource.label.description' => 'Deskripsi',
+    'resource.label.type' => 'Jenis',
+    'resource.label.event' => 'Kegiatan',
+    'resource.label.logged_at' => 'Tercatat pada',
+    'resource.label.properties' => 'Properti',
+    'resource.label.old' => 'Lama',
+    'resource.label.new' => 'Baru',
+    'resource.label.old_value' => 'Nilai Lama',
+    'resource.label.new_value' => 'Nilai Baru',
+];
