8b9b58279cfd14f27d08cf91a9334ffc19ccaa4f
Nick Gejadze <gejadze@gmail.com>
2023-06-10 01:14:40 +0400

    Georgian Translation

---
+++ resources/lang/ka/filament-logger.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'პარამეტრები',
+    'nav.log.label' => 'საქმიანობის ჟურნალი',
+    'nav.log.icon' => 'heroicon-o-clipboard-list',
+    'resource.label.log' => 'საქმიანობის ჟურნალი',
+    'resource.label.logs' => 'საქმიანობის ჟურნალი',
+    'resource.label.user' => 'მომხმარებელი',
+    'resource.label.subject' => 'საგანი',
+    'resource.label.subject_type' => 'საგნის ტიპი',
+    'resource.label.description' => 'აღწერილობა',
+    'resource.label.type' => 'ტიპი',
+    'resource.label.event' => 'ქმედება',
+    'resource.label.logged_at' => 'ავტორიზირებული როგორც',
+    'resource.label.properties' => 'Თვისებები',
+    'resource.label.old' => 'ძველი',
+    'resource.label.new' => 'ახალი',
+    'resource.label.old_value' => 'ძველი მნიშვნელობა',
+    'resource.label.new_value' => 'ახალი მნიშვნელობა',
+];
