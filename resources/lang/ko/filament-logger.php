6405a25c9887bd56f077ff0d5d61e9f3718a851e
corean <corean@corean.biz>
2024-04-05 00:16:54 +0900

    - Korean translations add

---
+++ resources/lang/ko/filament-logger.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => '설정',
+    'nav.log.label' => '활동 로그',
+    'nav.log.icon' => 'heroicon-o-clipboard-document-list',
+    'resource.label.log' => '활동 로그',
+    'resource.label.logs' => '활동 로그',
+    'resource.label.user' => '사용자',
+    'resource.label.subject' => '주제',
+    'resource.label.subject_type' => '주제 유형',
+    'resource.label.description' => '설명',
+    'resource.label.type' => '유형',
+    'resource.label.event' => '이벤트',
+    'resource.label.logged_at' => '기록된 시간',
+    'resource.label.properties' => '속성',
+    'resource.label.old' => '이전',
+    'resource.label.new' => '새로운',
+    'resource.label.old_value' => '이전 값',
+    'resource.label.new_value' => '새로운 값',
+    'resource.label.properties_hint' => '키 또는 값이 될 수 있습니다',
+    'resource.label.old_attributes' => '이전 속성 또는 값: ',
+    'resource.label.new_attributes' => '새로운 속성 또는 값: ',
+];
