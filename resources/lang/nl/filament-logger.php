d2d84a780b3a0b80066cfc19d4584e05f0c209a3
Daniël <78355193+Daniel-H123@users.noreply.github.com>
2024-04-07 14:16:28 +0200

    [lang] Add Dutch translations (#96)
    * lang: added Dutch translations
    
    * fix: translation typo
    
    * fix: improved translated sentence

---
+++ resources/lang/nl/filament-logger.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Instellingen',
+    'nav.log.label' => 'Activiteitenlog',
+    'nav.log.icon' => 'heroicon-o-clipboard-document-list',
+    'resource.label.log' => 'Activiteiten log',
+    'resource.label.logs' => 'Activiteiten logs',
+    'resource.label.user' => 'Gebruiker',
+    'resource.label.subject' => 'Onderwerp',
+    'resource.label.subject_type' => 'Onderwerp type',
+    'resource.label.description' => 'Beschrijving',
+    'resource.label.type' => 'Type',
+    'resource.label.event' => 'Gebeurtenis',
+    'resource.label.logged_at' => 'Gelogd op',
+    'resource.label.properties' => 'Eigenschappen',
+    'resource.label.old' => 'Oud',
+    'resource.label.new' => 'Nieuw',
+    'resource.label.old_value' => 'Oude waarde',
+    'resource.label.new_value' => 'Nieuwe waarde',
+    'resource.label.properties_hint' => 'Kan sleutel of waarde zijn',
+    'resource.label.old_attributes' => 'Oude attribuut of waarde: ',
+    'resource.label.new_attributes' => 'Nieuwe attribuut of waarde: ',
+];
