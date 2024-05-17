14438906d67dd0b966a222f4ff6bada1a4dfc56c
Ján Hamrák <snickom@gmail.com>
2024-05-17 12:36:30 +0200

    Slovak translation

---
+++ resources/lang/sk/filament-logger.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigácia & Zdroje
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Nastavenia',
+    'nav.log.label' => 'Záznam aktivity',
+    'nav.log.icon' => 'heroicon-o-clipboard-document-list',
+    'resource.label.log' => 'Záznam aktivity',
+    'resource.label.logs' => 'Záznamy aktivity',
+    'resource.label.user' => 'Používateľ',
+    'resource.label.subject' => 'Predmet',
+    'resource.label.subject_type' => 'Typ predmetu',
+    'resource.label.description' => 'Popis',
+    'resource.label.type' => 'Typ',
+    'resource.label.event' => 'Udalosť',
+    'resource.label.logged_at' => 'Zaznamenané',
+    'resource.label.properties' => 'Vlastnosti',
+    'resource.label.old' => 'Starý',
+    'resource.label.new' => 'Nový',
+    'resource.label.old_value' => 'Stará hodnota',
+    'resource.label.new_value' => 'Nová hodnota',
+    'resource.label.properties_hint' => 'Môže byť kľúč alebo hodnota',
+    'resource.label.old_attributes' => 'Starý atribút alebo hodnota: ',
+    'resource.label.new_attributes' => 'Nový atribút alebo hodnota: ',
+];
