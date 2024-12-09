a22bcb511b8b1c7ac36536a4339e9a3baaef6113
robertjunek <46076555+robertjunek@users.noreply.github.com>
2024-12-09 12:30:22 +0100

    Czech translation
    Added folder/file with Czech translation

---
+++ resources/lang/cs/filament-logger.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Navigation & Resource
+    |--------------------------------------------------------------------------
+    */
+
+    'nav.group' => 'Nastavení',
+    'nav.log.label' => 'Záznam aktivit',
+    'nav.log.icon' => 'heroicon-o-clipboard-document-list',
+    'resource.label.log' => 'Záznam aktivit',
+    'resource.label.logs' => 'Záznamy aktivit',
+    'resource.label.user' => 'Uživatel',
+    'resource.label.subject' => 'Subjekt',
+    'resource.label.subject_type' => 'Typ subjektu',
+    'resource.label.description' => 'Popis',
+    'resource.label.type' => 'Typ',
+    'resource.label.event' => 'Událost',
+    'resource.label.logged_at' => 'Zaznamenáno v',
+    'resource.label.properties' => 'Vlastnosti',
+    'resource.label.old' => 'Původní',
+    'resource.label.new' => 'Nové',
+    'resource.label.old_value' => 'Původní hodnota',
+    'resource.label.new_value' => 'Nová hodnota',
+    'resource.label.properties_hint' => 'Může být klíč nebo hodnota',
+    'resource.label.old_attributes' => 'Původní atribut nebo hodnota: ',
+    'resource.label.new_attributes' => 'Nový atribut nebo hodnota: ',
+];
